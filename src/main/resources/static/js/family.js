d1bc5eeaaa3100c70304a85653d88fdd50dd7a9b
sunghoan-we <javarajava@hotmail.com>
2021-04-05 03:25:38 -0700

    add Checking Session register Family Information

--- src/main/resources/static/js/family.js
+++ src/main/resources/static/js/family.js
@@ -1,3 +1,15 @@
+let index = 1;
 function addChildInfo(){
-
+    let childInfo = document.getElementById("childrenInfo");
+    let childInfoItems = "<label htmlFor=''>Child's Name</label>"
+        +"<input type='text' name='children["+index+"].name' placeholder='name of your child' value='child name'>"
+        +"<label htmlFor=''>Child's Gender</label>"
+        +"<select name='children["+index+"].gender' id='gender'>"
+        +"<option value='boy'>boy</option>"
+        +"<option value='girl'>girl</option>"
+        +"</select>"
+        +"<label htmlFor=''>Date of Birth</label>"
+        +"<input type='date' value='2021-04-09' name='children["+index+"].dateOfBirth'>";
+    childInfo.innerHTML += childInfoItems;
+    index++;
 }
