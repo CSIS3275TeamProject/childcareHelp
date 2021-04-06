0866096f67988c424b7ea20a444d513a75e9b0cf
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-04-06 14:55:55 -0700

    make search bar fully functional. remove age filter

--- src/main/resources/static/js/searchBabysitter.js
+++ src/main/resources/static/js/searchBabysitter.js
@@ -2,8 +2,7 @@ const applyFilters = () => {
     let inputText = document.getElementById("key").value;
     let gender = document.getElementById("gender").value;
     let degree = document.getElementById("degree").value;
-    let age = document.getElementById("age").value;
-    let uri = `/babysitter/listOfBabysitters/${inputText}/${gender}/${degree}/${age}`;
+    let uri = `/babysitter/listOfBabysitters/name=${inputText}/gender=${gender}/degree=${degree}`;
     let encorded = encodeURI(uri);
     document.getElementById("btn-apply").href=encorded;
     return false;
